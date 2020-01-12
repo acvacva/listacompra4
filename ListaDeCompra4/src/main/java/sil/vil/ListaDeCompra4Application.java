78aa4f2e7b33d51b92d4d1698680da95d819510a
acvacva <acvacva@gmail.com>
2020-01-12 15:07:44 +0100

    firsttttttttttttttt

---
+++ ListaDeCompra4/src/main/java/sil/vil/ListaDeCompra4Application.java
@@ -0,0 +1,13 @@
+package sil.vil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeCompra4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeCompra4Application.class, args);
+	}
+
+}
---
+++ ListaDeCompra4/src/test/java/sil/vil/ListaDeCompra4ApplicationTests.java
@@ -0,0 +1,13 @@
+package sil.vil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeCompra4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
