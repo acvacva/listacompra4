a0f30337bcdefe59070d0d0fe0361a2f2261c224
sirivera <sirivera@N-5CG62309QR.nsn-intra.net>
2020-01-12 15:43:26 +0100

    update de borrame con getters

--- ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
+++ ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
@@ -1,5 +1,13 @@
 package sil.vil.controlador;
 
 public class borrame {
+private String comida;
 
+public String getComida() {
+	return comida;
+}
+
+public void setComida(String comida) {
+	this.comida = comida;
+}
 }
