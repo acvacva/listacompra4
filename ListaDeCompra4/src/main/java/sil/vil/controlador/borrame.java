5c30b33dbfff16696decb7266dcddd56dc36ba9c
sirivera <sirivera@N-5CG62309QR.nsn-intra.net>
2020-01-12 15:57:07 +0100

    conflicto

--- ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
+++ ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
@@ -6,6 +6,20 @@ private String comida;
 public String getComida() {
 	return comida;
 }
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
 
 public void setComida(String comida) {
 	this.comida = comida;
