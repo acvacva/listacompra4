4d0772b76179a78a968a47ee691eac598a0edd43
acvacva <acvacva@gmail.com>
2020-01-12 15:41:25 +0100

    nombre

--- ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
+++ ListaDeCompra4/src/main/java/sil/vil/controlador/borrame.java
@@ -1,5 +1,17 @@
 package sil.vil.controlador;
 
 public class borrame {
+	
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 
 }
